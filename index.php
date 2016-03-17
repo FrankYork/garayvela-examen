f6b0cf138c484636428254ca56865dad21fd223f
Alumno <Alumno>
2016-03-17 17:15:06 -0500

    examen

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>CodeYork</title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	
+</head>
+<body background="public/imagen/12B.jpg"> 
+
+<div class="container">
+
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+
+
+			<div class="navbar-header">
+
+			 <img src="" width="50">
+
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<a class="navbar-brand" href="#">CodeYork</a>
+			</div>
+			
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="nav navbar-nav navbar-right"><a target="_bank" href="">Registrate</a></li>
+				</ul>
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Escribe Aqui">
+
+					</div>
+
+					<button type="submit" class="btn btn-default">BUSCAR</button>
+				</form>
+				 
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"></a></li>
+					<li class="dropdown">
+						<a href="#" class="navbar-brand" data-toggle="dropdown">Opciones <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+						 	<li><a href="#">Galeria</a></li>
+							<li><a href="#">Videos</a></li>
+							<li><a href="#">Novedades</a></li>
+							<li><a href="#">Configuracion</a></li>
+
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+		   
+	 </nav>
+</div>
+
+
+<center>
+
+<div id="carousel-id" class="carousel slide" data-ride="">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-id" data-slide-to="" class="active"></li>
+		<li data-target="#carousel-id" data-slide-to="" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="" class=""></li>
+		
+	</ol>
+	<div class="carousel-inner">
+	
+		<div class="item active">
+			<img data-src="public/imagen/fotos-de-portada-para-facebook-1.jpg" alt="" src="public/imagen/fotos-de-portada-para-facebook-1.jpg" width="1224">
+			<div class="container">
+				<div class="carousel-caption">
+					
+			    	<p><a class="btn btn-lg btn-primary" href="#" role="button">Dragon Ball Z</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<img data-src="public/imagen/portada-con-frase-para-facebook_124.jpg" alt="" src="public/imagen/portada-con-frase-para-facebook_124.jpg" width="1224">
+			<div class="container">
+				<div class="carousel-caption">
+					
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">The Lost Canvas</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<img data-src="public/imagen/psicodelico-portada-facebook.jpg" alt="" src="public/imagen/psicodelico-portada-facebook.jpg" width="1225">
+			<div class="container">
+				<div class="carousel-caption">
+					
+					
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Tokio Ghoul</a></p>
+				</div>
+			</div>
+		</div>
+		
+	 </div>
+	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+
+</div>
+</center>
+
+<script src="public/js/jquery-1.12.1.js"></script>
+<script src="public/js/bootstrap.js"></script>
+
+
+
+</body>
+</html>
